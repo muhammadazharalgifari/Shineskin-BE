f2f5345b3f06c68e37348b71d5d014d256237ed9
fathurohmansf <fathurrhm909@gmail.com>
2024-12-26 16:30:39 +0700

    add get Transaction by Auth

--- src/controllers/transaction/getTransactionByAuth.js
+++ src/controllers/transaction/getTransactionByAuth.js
@@ -7,15 +7,6 @@ async function getTransactionByAuth(req = request, res = response) {
     // current user
     const userId = req.userId;
 
-    const response = await db.users.findUnique({
-      where: {
-        id: parseInt(userId),
-      },
-      select: {
-        id: true,
-        cartItem: true
-      },
-    });
 
     if (isNaN(userId)) {
       return res.status(400).json({
@@ -24,25 +15,21 @@ async function getTransactionByAuth(req = request, res = response) {
       });
     }
 
-    if (!response) {
-      return res.status(404).json({
-        status: "error",
-        message: `Cart item with User ID ${userId} not found`,
-      });
-    }
-
     // ambil data subtotal terbaru
-    const totalPrice = await db.transactions.findFirst({
+    const response = await db.transactions.findFirst({
       where: {
         userId: parseInt(userId),
-        status: "PENDING",
       },
       select: {
+        id: true,
+        status: true,
         total_price: true,
+        createdAt: true,
+        updatedAt: true
       },
     });
 
-    if (!totalPrice) {
+    if (!response) {
       return res.status(404).json({
         status: "error",
         message: `Transaction NOT Found with User ID ${userId} `,
@@ -51,7 +38,7 @@ async function getTransactionByAuth(req = request, res = response) {
 
     res.status(200).json({
       status: "success",
-      total_price: totalPrice.total_price
+      data: response,
     });
   } catch (error) {
     console.log(error);
