851d55ac7c699ec70eeca9f3ccdec39e010ecfbb
fathurohmansf <fathurrhm909@gmail.com>
2025-01-09 15:04:20 +0700

    add set lunas / succes & include product formatter transaction

--- src/controllers/transaction/getAllTransactions.js
+++ src/controllers/transaction/getAllTransactions.js
@@ -28,9 +28,37 @@ async function getAllTransactions(req = request, res = response) {
         }
       },
     });
+
+    // Formatter Transaction
+    const formatterTransaction = response.map((item) => {
+      return {
+        id: item.id,
+        user: {
+          username: item.user.username,
+          email: item.user.email,
+        },
+        total_price: item.total_price,
+        status: item.status,
+        imageTransaction: item.imageTransaction,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt,
+        products: item.cartItems.map((cartItem) => {
+          return {
+            id: cartItem.product.id,
+            name: cartItem.product.name,
+            price: cartItem.product.price,
+            imageProduct: cartItem.product.imageProduct,
+            quantity: cartItem.quantity,
+            subtotal_price: cartItem.subtotal_price,
+          };
+        }),
+      };
+    });
+
+
     res.status(200).json({
       status: "success",
-      data: response,
+      data: formatterTransaction,
     });
   } catch (error) {
     console.log(error);
